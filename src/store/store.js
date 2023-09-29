ddd652e27ad5d9f2155297b78a3cc56d760ef605
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-09-28 20:52:24 -0600

    Creating the store

---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { counterSlice } from './slices/counter/counterSlice';
+
+export const store = configureStore({
+    reducer: {
+        counter: counterSlice.reducer,
+
+
+
+    },
+});
