c75875f9e89f4db3f4a52c748e80e6ceeabdb638
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-09-29 18:54:55 -0600

    Adding new App with new store

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const PokemonApp = () => {
+    return (
+        <>
+            <h1>Pokemon App</h1>
+            <hr />
+
+            <ul>
+                <li>Hola 1</li>
+                <li>Hola 1</li>
+                <li>Hola 1</li>
+            </ul>
+        </>
+    )
+}
+
+export default PokemonApp
--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './store/store';
+import PokemonApp from './PokemonApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      {/* <App /> */}
+      <PokemonApp />
     </Provider>
   </React.StrictMode>
 );
