27d03f5b5eef3dcd0dcd5f225d73367df73b4b78
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-10-01 19:12:32 -0600

    Adding RTK Query

---
+++ src/TodoApp.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const TodoApp = () => {
+  return (
+    <>
+    
+        <h1>Todo - RTK Query</h1>
+        <hr />
+        <h4>isLoading...</h4>
+        <h4>isFetching...</h4>
+        
+        <pre>...</pre>
+
+        <button>
+            Next To Do
+        </button>
+
+    
+    </>
+  )
+}
+
+export default TodoApp
--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './store/store';
-import PokemonApp from './PokemonApp';
+import TodoApp from './TodoApp';
+//import PokemonApp from './PokemonApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <App /> */}
-      <PokemonApp />
+      {/* <PokemonApp /> */}
+      <TodoApp />
+     
     </Provider>
   </React.StrictMode>
 );
