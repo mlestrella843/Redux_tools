48b43d5f4038692b00f05185d24a10b96a5e3630
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-10-03 07:36:03 -0600

    Adding controls

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,18 +1,51 @@
-import React from 'react'
-import { useGetTodosQuery } from './store/apis/todosApis'
+import React, { useState } from 'react'
+import { useGetTodosQuery, useGetTodosByIdQuery } from './store/apis/todosApis'
 
 const TodoApp = () => {
 
-     const algo = useGetTodosQuery();
+  // useState to control the button 
+  const [todoId, setTodoId] = useState(1);
+
+  const nexToDo = () => {
+    setTodoId(todoId + 1 );
+  }
+
+  const previusToDo = () => {
+    if( todoId ===1 ) return;
+    setTodoId(todoId - 1 );
+  }
+      //Show all todos
+      //  const { data: todos=[] , isLoading } = useGetTodosQuery();
+      //  console.log(algo);
+
+      //show todo by Id
+      const { data: todo, isLoading } = useGetTodosByIdQuery(todoId);
+      //console.log(todo);
+
 
   return (
     <>  
         <h1>Todos - RTK Query</h1>
         <hr />
-        <h4>isLoading...</h4>
-        <pre>...</pre>
+        <h4>isLoading: { isLoading ? 'True' : 'False' }</h4>
+       
+       
+        <pre>{ JSON.stringify( todo ) }</pre>
 
-        <button>
+        {/* <ul>
+            { todos.map( todo => (         
+                <li key={todo.id}>
+                    <strong> { todo.completed ? 'DONE' : 'Pending'} </strong>
+                    {todo.title}
+                </li>   
+            ))}
+        </ul> */}
+
+        <button onClick={ previusToDo }>
+            Previus To Do
+        </button>
+        
+        <button onClick={ nexToDo }>
             Next To Do
         </button>
     </>
--- src/store/apis/todosApis.js
+++ src/store/apis/todosApis.js
@@ -2,14 +2,22 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const todosApis = createApi({
+
     reducerPath: 'todos',
+
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://jsonplaceholder.typicode.com'
     }),
+
     endpoints: (builder) => ({
+
         getTodos: builder.query({
             query: () => '/todos'
+        }),
+        
+        getTodosById: builder.query({
+            query: (todoId) => `/todos/${todoId}`
         })
     })
 })
-export const { useGetTodosQuery } = todosApis;
+export const { useGetTodosQuery, useGetTodosByIdQuery } = todosApis;
